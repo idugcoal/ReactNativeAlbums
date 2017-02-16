f1aa374709d037d1766bbde37af9c7ce80b42384
Doug Cole <idugcoal@gmail.com>
2017-02-16 15:57:29 -0800

    obsfucates password input

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import { config } from '../config.js';
+import LoginForm from './components/LoginForm'
 
 class App extends Component {
+	componentWillMount() {
+		firebase.initializeApp(config);
+	}
+
 	render() {
 		return (
 			<View>
 				<Header headerText="Authentication" />
-					<Text>Hello</Text>
+					<LoginForm />
 			</View>
 		);
 	}
