f81b580fbdfc5af69098abc3a63578dc386d8f19
Doug Cole <idugcoal@gmail.com>
2017-02-16 17:30:36 -0800

    adds login and logout

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,23 +1,49 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
-import { config } from '../config.js';
-import LoginForm from './components/LoginForm'
+import { Header, Button, Spinner, CardSection } from './components/common';
+import LoginForm from './components/LoginForm';
+import { config } from '../config';
 
 class App extends Component {
-	componentWillMount() {
-		firebase.initializeApp(config);
-	}
+  state = { loggedIn: null };
 
-	render() {
-		return (
-			<View>
-				<Header headerText="Authentication" />
-					<LoginForm />
-			</View>
-		);
-	}
+  componentWillMount() {
+    firebase.initializeApp(config);
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+        	<CardSection>
+        		<Button onPress={() => firebase.auth().signOut()}>
+        			Log Out
+        		</Button>
+        	</CardSection>);
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
+  }
+
+  render() {
+    return (
+      <View>
+        <Header headerText="Authentication" />
+        {this.renderContent()}
+      </View>
+    );
+  }
 }
 
 export default App;
